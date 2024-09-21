15da6b9c3cffd1592c6a7d9729865fd822372a8f
Your Name <you@example.com>
2024-09-21 14:51:22 -0500

    update to print statemetns

--- src/inference_helper.cpp
+++ src/inference_helper.cpp
@@ -783,7 +783,7 @@ bool InferenceHelper::postprocess_object_detect(
             if (en_debug)
             {
                 printf("Detected: %s, Confidence: %6.2f\n",
-                    labels[detected_classes[i]].c_str(), (double)score);
+                       labels[detected_classes[i]].c_str(), (double)score);
             }
             int height = bottom - top;
             int width = right - left;
@@ -792,7 +792,7 @@ bool InferenceHelper::postprocess_object_detect(
             cv::Point pt(left, top - 10);
 
             cv::rectangle(output_image, rect,
-                        get_color_from_id(detected_classes[i]), 2);
+                           get_color_from_id(detected_classes[i]), 2);
             cv::putText(output_image, labels[detected_classes[i]], pt,
                         cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(0), 2);
 
