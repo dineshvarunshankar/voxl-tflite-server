0e958d162ef8e687d86214692e1ea6d4f1c0d606
Aditya Dandwate <adityadandwate24@gmail.com>
2025-03-26 11:36:19 +0000

    Edit build.sh

--- build.sh
+++ build.sh
@@ -47,7 +47,7 @@ case "$1" in
 		mkdir -p build
 		cd build
 		cmake -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_QRB5165} -DCMAKE_BUILD_TYPE=RELEASE -DBUILD_QRB5165=${BUILD_QRB5165} -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++14 -march=armv8-a" ${EXTRA_OPTS} ../
-		make -j$(( $(nproc) / 2 ))
+		make -j$(nproc)
 		cd ../
 		;;
 	native)
